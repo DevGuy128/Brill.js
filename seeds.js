3a9df872c4c47e2c55f786933c44ecf92dc5cc50
DevGuy128 <DevGuy128@users.noreply.github.com>
2014-12-07 20:05:28 +0000

    Create seeds.js

---
+++ seeds.js
@@ -0,0 +1,3 @@
+//Here is an example:
+//var users=[{name:"example",highscore:highscores[0]},{name:"example2",highscore:highscores[1]}];
+//var highscores=[{value:2000,user:users[0]},{value:1000,user:users[1]}]
