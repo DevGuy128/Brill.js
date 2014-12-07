58e0390375fae6bb8665a07d6d89dc441d85d82a
DevGuy128 <DevGuy128@users.noreply.github.com>
2014-12-07 19:27:03 +0000

    Update Brill.js

--- Brill.js
+++ Brill.js
@@ -154,33 +154,6 @@ window.brill = (function() {
             };
         }
     }());
-    brill.prototype.get = function(endpoint,callback){
-      var xmlHttp = null;
-
-      xmlHttp = new XMLHttpRequest();
-      xmlHttp.open( "GET", endpoint, false );
-      xmlHttp.send( null );
-      return xmlHttp.responseText;
-      callback.call();
-    };
-    brill.prototype.post = function(endpoint,callback){
-      var xmlHttp = null;
-
-      xmlHttp = new XMLHttpRequest();
-      xmlHttp.open( "POST", endpoint, false );
-      xmlHttp.send( null );
-      return xmlHttp.responseText;
-      callback.call();
-    };
-    brill.prototype.delete = function(endpoint,callback){
-      var xmlHttp = null;
-
-      xmlHttp = new XMLHttpRequest();
-      xmlHttp.open( "DELETE", endpoint, false );
-      xmlHttp.send( null );
-      return xmlHttp.responseText;
-      callback.call();
-    };
     
     var briller = {
         get: function(selector) {
