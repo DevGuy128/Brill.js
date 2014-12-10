357977c8eec66ce1fc4ed84540fc1233be9eac67
DevGuy128 <DevGuy128@users.noreply.github.com>
2014-12-10 20:07:20 +0000

    Delete seeds.js

--- seeds.js
+++
@@ -1,3 +0,0 @@
-//Here is an example:
-//var users=[{name:"example",highscore:highscores[0]},{name:"example2",highscore:highscores[1]}];
-//var highscores=[{value:2000,user:users[0]},{value:1000,user:users[1]}]
