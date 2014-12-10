388f0c56975a336d0e45d3535cae4cc4694ab36c
DevGuy128 <DevGuy128@users.noreply.github.com>
2014-12-10 20:11:07 +0000

    Update unit.js

--- tests/unit.js
+++ tests/unit.js
@@ -1,1 +1,11 @@
 //Tests go here
+try{
+  var app = require("app.js");
+  if(app !== undefined || null){
+    throw "App is empty".
+  }
+  
+}catch(error){
+  console.log("Error: " + error.message + ".");
+}
+
