7263cd6f246fada2d0339334547f7113e5b34c0f
DevGuy128 <DevGuy128@users.noreply.github.com>
2014-12-10 20:07:07 +0000

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,11 +1,9 @@
 try{
   var app = require("app.js");
-  if(app.forEach){
-    throw "forEach has been deprecated. Please use map.";
-  }
-  if(app !== undefined){
+  if(app !== undefined || null){
     throw "App is empty".
   }
+  
 }catch(error){
   console.log("Error: " + error.message + ".");
 }
