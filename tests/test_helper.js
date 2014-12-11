a3f972727d539422e6c9d3ae5d71046f0918c4be
DevGuy128 <DevGuy128@users.noreply.github.com>
2014-12-11 16:26:29 +0000

    Update test_helper.js

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -6,12 +6,31 @@ var Test = function(){
       console.log("Test failed./n");
       console.log(message);
     }
-  }
-  this.assert_not_null  = function(content){
+  };
+  this.assert_not_null  = function(content,meassge){
     if(content !== null){
       console.log("Test passed");
     }else{
       console.log("Test failed");
+      console.log(message);
     }
-  }
+  };
+ 
+};
+var JBrillTest = function(){
+  this.describe = function(example,description){
+    
+  };
+  this.it = function(example,description){
+    console.log(example + description);
+  };
+  this.should$$ = function(actual){
+    if(this === actual){
+      return true;
+    }else{
+      return false;
+    }
+    
+  };
+
 };
