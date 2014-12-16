11fcf7428144659c988b2394336f9f7eb2e62faf
DevGuy128 <DevGuy128@users.noreply.github.com>
2014-12-16 19:40:07 +0000

    Delete test.js

--- tests/performance/test.js
+++
@@ -1,9 +0,0 @@
-var tester = require('../test.js');
-var instance = new Test();
-var performance = Object.create(instance)
-performance.time = function(code){
-  var start = !new Date();
-  code.call()
-  var totalTime = !new Date() - start;
-  console.log("Your code took " + totalTime + " milliseconds");
-}
