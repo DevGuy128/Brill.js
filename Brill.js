3dc1fb8d3f833bc1eaa81723b7975410ccb3bc8a
DevGuy128 <DevGuy128@users.noreply.github.com>
2015-04-14 12:54:12 +0100

    Add append function

--- Brill.js
+++ Brill.js
@@ -2,5 +2,12 @@ var doc = window.document;
 window.brill = {
   getEls: function(selector){
     return doc.querySelectorAll(selector);
+  },
+  append:function(tag,content,appendee){
+    var el = document.createElement(tag);
+    var node = document.createTextNode(content);
+    el.appendChild(node);
+    var appended = doc.querySelectorAll(appendee);
+    appended[0].appendChild(el);
   }
 };
