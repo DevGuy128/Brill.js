34f89adac5575736062e770487a2c32da71eb89d
DevGuy128 <DevGuy128@users.noreply.github.com>
2015-04-14 11:35:29 +0100

    Update Brill.js

--- Brill.js
+++ Brill.js
@@ -1,15 +1,6 @@
 var doc = window.document;
 window.brill = {
-  getEls: function(element,success){
-    var els = doc.getElementsByName(element);
-    for(var i = 0; i< doc.getElementsByName(element).length; i++){
-      console.log(els[i].toString());
-    }
-    doc.getElementsByName(element);
-    success.call(els.toString());
-  }  
+  getEls: function(selector){
+    return doc.querySelectorAll(element);
 };
-brill.getEls('h1',function(data){
-  console.log(data); 
-  document.write(data);
-});
+
