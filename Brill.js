ec211059d89b5b78add4474d905bc1b4aefe5228
DevGuy128 <DevGuy128@users.noreply.github.com>
2015-04-20 14:48:48 -0400

    Add improved selector and add hide function

--- Brill.js
+++ Brill.js
@@ -1,13 +1,15 @@
 var doc = window.document;
-window.brill = {
-  getEls: function(selector){
-    return doc.querySelectorAll(selector);
-  },
-  append:function(tag,content,appendee){
+window.append = function(tag,content,appendee){
     var el = document.createElement(tag);
     var node = document.createTextNode(content);
     el.appendChild(node);
     var appended = doc.querySelectorAll(appendee);
     appended[0].appendChild(el);
-  }
 };
+window.$ = function(selector){
+  return doc.querySelectorAll(selector);
+};
+
+window.hide = function(selector){
+  doc.querySelectorAll(selector)[0].style.visibility = "hidden";
+}
